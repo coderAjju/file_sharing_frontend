2f80ca82f39e772d69a63eac98348205beaa235b
ajay <ajay@localhost.localdomain>
2024-10-01 16:34:44 +0530

    files updated

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const RegistrationForm = () => {
